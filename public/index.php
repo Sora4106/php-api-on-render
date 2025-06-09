4592afb53b99aa3818496f72bdd9026a495b58df
Sora4106 <sora4106@gmail.com>
2025-06-09 10:39:52 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,7 +1,13 @@
 <?php
 header('Content-Type: application/json');
+
 $data = [
-  ["id" => "1", "name" => "10", "age" => "2020"]
+    ["id" => "1", "name" => "10", "age" => "2020"],
+    ["id" => "2", "name" => "20", "age" => "2021"],
+    ["id" => "3", "name" => "30", "age" => "2022"]
 ];
+
 echo json_encode($data);
 ?>
+
+<?php
