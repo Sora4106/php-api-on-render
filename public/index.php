911f59816f3c85e84dae5c8fab203139f772c203
Sora4106 <sora4106@gmail.com>
2025-06-09 12:51:20 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,13 +1,14 @@
 <?php
-$url = "https://myapi.infinityfreeapp.com/?";
-
-$options = [
-    "http" => [
-        "method" => "GET",
-        "header" => "User-Agent: Mozilla/5.0\r\n"
-    ]
+$opts = [
+  "http" => [
+    "method" => "GET",
+    "header" => "User-Agent: Mozilla/5.0\r\n"
+  ]
 ];
-$context = stream_context_create($options);
+$context = stream_context_create($opts);
+
+// ⚠ 目標網站可能會阻擋 curl，改用 file_get_contents + UA spoofing
+$url = "https://myapi.infinityfreeapp.com/?i=1";
 $response = file_get_contents($url, false, $context);
 
 header('Content-Type: application/json');
