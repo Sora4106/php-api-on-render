a1685e405eebb60c51393225f81f7c8aa9070bcc
Sora4106 <sora4106@gmail.com>
2025-06-09 16:22:27 +0800

    Delete .render directory

--- .render/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt-get update
-
-# 安裝 Apache、PHP 及 mysqli 模組
-apt-get install -y apache2 php libapache2-mod-php php-mysqli
-
-# 將 public 裡的檔案複製到 Apache 的網頁根目錄
-cp -r public/* /var/www/html
